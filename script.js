97d5b16a12629c25dc2487a5a84648f51de15876
zainabismailova1603 <is1972mailova@gmail.com>
2021-12-09 20:58:13 +0300

    container

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log('Privet');
-console.log('zainab');
+// console.log('Privet');
+// console.log('zainab');
+// cconvertToDec([0, 1, 0, 0, 1, 1, 0, 1]);
+// if(arrLength !==8){
+//     return;
+// }
+// let pow = 7;
+// let decimalRes = 0;
+// arrNum.forEach; element=>{
+//     decimalRes += element * Mach.pow(2, pow);
+//    pow --;
+// })
+// return decimalRes;
+// }
+// const decimalNum = convertToDec([0, 1, 0, 0, 1, 1, 0, 1]);
+// console.log(decimalNum);
+// const quote = "Anything's possible if you've got enough nerve.";
+
+// 
